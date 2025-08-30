a405101e8275891c86a66f5501ba40a4e73447f5
Imran0316 <ik775239@gmail.com>
2025-08-29 19:08:56 -0700

    database model users

---
+++ DvdStore/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.ComponentModel.DataAnnotations;
+
+namespace DvdStore.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public int AlbumId { get; set; }
+        public int SuplierId { get; set; }
+        public int ProducerId { get; set; }
+    }
+}
---
+++ DvdStore/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DvdStore.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime Created_At { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+}
